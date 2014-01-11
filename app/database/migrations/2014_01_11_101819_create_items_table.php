1a7ec3e70d6f9405fc9e7cab84898886cef96693
Kuurde <kurt.taffein@gmail.com>
2014-01-11 16:12:00 +0100

    Items schema created.

---
+++ app/database/migrations/2014_01_11_101819_create_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('items', function($table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('amount');
+			$table->text('description')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('items');
+	}
+
+}

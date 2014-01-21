a7a369a926b0c1e9957fb83f41a433e8e28f85e9
Kuurde <kurt.taffein@gmail.com>
2014-01-21 20:17:00 +0100

    Validate function added to the Item model.

--- app/models/Item.php
+++ app/models/Item.php
@@ -7,4 +7,12 @@ class Item extends Eloquent {
 	protected $fillable = array('name', 'amount', 'description');
 	protected $guarded = array('id');
 	
+	public static function validate($input) {
+		$rules = array(
+			'name' => 'Required',
+			'amount' => 'Required|Integer|Min:1'
+		);
+		
+		return Validator::make($input, $rules);
+	}
 }

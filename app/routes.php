9ab7066f9aaea0b9b467b0743647668f3565bfe7
Kuurde <kurt.taffein@gmail.com>
2014-02-01 12:21:38 +0100

    Added redirect to '/' route to '/list'.

--- app/routes.php
+++ app/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return Redirect::to('list');
 });
 
 Route::get('list', array('as' => 'list', 'before' => 'auth.basic', 'uses' => 'ItemController@listItems'));

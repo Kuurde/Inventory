f42a22150c7a0d8a64bfcd1a8fd9ea77838f18f4
Kuurde <kurt.taffein@gmail.com>
2014-02-02 18:57:54 +0100

    Fixed css issue by using CDN link in master.blade.php

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,13 +2,12 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
 	</head>
 	<body>
 		<div class="container">
 			<h1>Inventory app</h1>
 			<div>@yield('content')</div>
 		</div>
-		<script src="js/bootstrap.js"></script>
 	</body>
 </html>
